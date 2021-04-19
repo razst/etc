367a10076c4c960ba7ccf02ad29e0f55bde060c2
Raz <raz_st@hotmail.com>
2021-04-19 11:18:34 +0300

    UDP projects for YCUbeSim
    UDPTRRX - is in WPF

---
+++ UDPTXRX/UDPTXRX/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UDPTXRX
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        UDPSocket server = new UDPSocket();
+        UDPSocket client = new UDPSocket();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string msg = "tmp:"+edtTemp.Text+ ",vbat:" + edtVbat.Text;
+            client.Send(msg);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            client.Client("127.0.0.1", 27002);
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            server.Server(27002, this);
+        }
+
+
+        public void updateData(string msg)
+        {
+            msg = msg.Trim();
+            txbRX.Text = txbRX.Text + msg + "\n";
+
+        }
+
+    }
+}
---
+++ UDPTXRX/UDPTXRX/UDPSocket.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace UDPTXRX
+{
+    public class UDPSocket
+    {
+        private Socket _socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        private const int bufSize = 8 * 1024;
+        private State state = new State();
+        private EndPoint epFrom = new IPEndPoint(IPAddress.Any, 0);
+        private AsyncCallback recv = null;
+        private MainWindow serverForm = null;
+
+        public class State
+        {
+            public byte[] buffer = new byte[bufSize];
+        }
+
+        public void Server(int port, MainWindow serverForm)
+        {
+            this.serverForm = serverForm;
+            _socket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.ReuseAddress, true);
+            _socket.EnableBroadcast = true;
+            // _socket.Bind(new IPEndPoint(IPAddress.Parse(address), port));
+            _socket.Bind(new IPEndPoint(IPAddress.Any, port));
+            Receive();
+        }
+
+        public void Client(string address, int port)
+        {
+            _socket.Connect(IPAddress.Parse(address), port);
+            Receive();
+        }
+
+        public void Send(string text)
+        {
+            byte[] data = Encoding.ASCII.GetBytes(text);
+            _socket.BeginSend(data, 0, data.Length, SocketFlags.None, (ar) =>
+            {
+                State so = (State)ar.AsyncState;
+                int bytes = _socket.EndSend(ar);
+                Console.WriteLine("SEND: {0}, {1}", bytes, text);
+            }, state);
+        }
+
+        private void Receive()
+        {
+            _socket.BeginReceiveFrom(state.buffer, 0, bufSize, SocketFlags.None, ref epFrom, recv = (ar) =>
+            {
+                State so = (State)ar.AsyncState;
+                int bytes = _socket.EndReceiveFrom(ar, ref epFrom);
+                _socket.BeginReceiveFrom(so.buffer, 0, bufSize, SocketFlags.None, ref epFrom, recv, so);
+                Console.WriteLine("RECV: {0}: {1}, {2}", epFrom.ToString(), bytes, Encoding.ASCII.GetString(so.buffer, 0, bytes));
+
+                serverForm.Dispatcher.Invoke(() => {
+                    serverForm.updateData(Encoding.ASCII.GetString(so.buffer, 0, bytes));
+                });
+
+            }, state);
+        }
+    }
+}
